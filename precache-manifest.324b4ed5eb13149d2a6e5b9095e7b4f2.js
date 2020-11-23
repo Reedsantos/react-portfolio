17bf41490423570cf5ef32a675f457fb0143ee3f
Reed Santos <reedsantos@icloud.com>
2020-11-23 13:19:39 -0500

    Updates

---
+++ precache-manifest.324b4ed5eb13149d2a6e5b9095e7b4f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c89937eb1be15716c6ad5ea27899c98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab19aacc27d9da1cfec3",
+    "url": "./static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "762a54384ff29c4c14fa",
+    "url": "./static/css/main.082fbf2e.chunk.css"
+  },
+  {
+    "revision": "ab19aacc27d9da1cfec3",
+    "url": "./static/js/2.646e4169.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.646e4169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762a54384ff29c4c14fa",
+    "url": "./static/js/main.9aa74bbd.chunk.js"
+  },
+  {
+    "revision": "d900f9fd8e1f352a9245",
+    "url": "./static/js/runtime-main.bb27c588.js"
+  },
+  {
+    "revision": "e974b3b07314334ffdb6554c69c04399",
+    "url": "./static/media/Profile.e974b3b0.jpg"
+  },
+  {
+    "revision": "4a20a27c3b9c3de1e9bcca79e46dc055",
+    "url": "./static/media/img9.4a20a27c.jpg"
+  }
+]);
